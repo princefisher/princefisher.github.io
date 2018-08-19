b6017bdddd448a12165014c489ddbdef1da18720
Constantin Ei <princefisher@gmx.de>
2018-08-18 19:34:51 -0700

    update

--- Test/🌗_files/🌗.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20180818
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('🌗_files/🌗Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
